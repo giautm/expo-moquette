06a27645c91d6f18b7db03f1a65b6d310ac11d45
Giau. Tran Minh <giau.tmg@gmail.com>
2022-10-24 15:00:48 +0700

    chore: cleanup (#5)
    * chore: remove example
    
    * chore: remove iOS

--- ios/ExpoMqttBroker.swift
+++
@@ -1,8 +0,0 @@
-@objc(ExpoMoquette)
-class ExpoMoquette: NSObject {
-
-    @objc(startServerAsync:withB:withResolver:withRejecter:)
-    func startServerAsync(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-        resolve(a*b)
-    }
-}
