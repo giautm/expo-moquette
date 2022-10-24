d232ff08acf06c02e0a33e3f1e734084450c76d6
Giau. Tran Minh <hello@giautm.dev>
2022-10-24 15:59:48 +0700

    fix: added client events
    Signed-off-by: Giau. Tran Minh <hello@giautm.dev>

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,6 @@
+import { useEffect } from 'react';
 import { NativeEventEmitter, NativeModules, Platform } from 'react-native';
-const ON_MESSAGE = 'ON_MESSAGE';
-export type Client = {
-  address: string;
-  port: number;
-  id: string;
-};
+
 const LINKING_ERROR =
   `The package 'expo-moquette' doesn't seem to be linked. Make sure: \n\n` +
   Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
@@ -22,6 +18,12 @@ const ExpoMoquette = NativeModules.ExpoMoquette
       }
     );
 
+export type Client = {
+  address: string;
+  port: number;
+  id: string;
+};
+
 type ServerConfig = {
   host: string;
   port: string;
@@ -30,6 +32,12 @@ type ServerConfig = {
   username?: string;
   nettyMaxBytes?: number;
 };
+
+export type ServerStatus = {
+  port: number;
+  sslPort: number;
+};
+
 export function startServerAsync(config: ServerConfig): Promise<string> {
   return ExpoMoquette.startServerAsync(config);
 }
@@ -46,11 +54,6 @@ export function stopServerAsync(): Promise<{ ok: Boolean }> {
   return ExpoMoquette.stopServerAsync();
 }
 
-export type ServerStatus = {
-  port: number;
-  sslPort: number;
-};
-
 export function getServerStatusAsync(): Promise<ServerStatus> {
   return ExpoMoquette.getServerStatusAsync();
 }
@@ -58,11 +61,39 @@ export function getServerStatusAsync(): Promise<ServerStatus> {
 export function isAvailable() {
   return Platform.OS === 'android';
 }
+
 const emitter = new NativeEventEmitter();
+
+const ON_CONNECT = 'ON_CONNECT';
+const ON_CONNECTION_LOST = 'ON_CONNECTION_LOST';
+const ON_DISCONNECT = 'ON_DISCONNECT';
+const ON_MESSAGE = 'ON_MESSAGE';
+
 export function subscribeTopic(topic: string, callback: (event: any) => void) {
   return emitter.addListener(ON_MESSAGE, (event: any) => {
     if (event.topic === topic || event.topic.startsWith(topic)) {
       callback(event);
     }
+  });
+}
+
+export function useClientEvent(handler: (event: any) => void) {
+  const createEventHandler = (eventName: string) => {
+    return (event: any) => {
+      handler({ event, eventName });
+    };
+  };
+  useEffect(() => {
+    const subs = [
+      emitter.addListener(ON_CONNECT, createEventHandler(ON_CONNECT)),
+      emitter.addListener(ON_DISCONNECT, createEventHandler(ON_DISCONNECT)),
+      emitter.addListener(
+        ON_CONNECTION_LOST,
+        createEventHandler(ON_CONNECTION_LOST)
+      ),
+    ];
+    return () => {
+      subs.forEach((sub) => sub.remove());
+    };
   });
 }
